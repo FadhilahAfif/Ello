1674e1b93bea2a0c10fd117efce277adfb8d847e
FadhilahAfif <fadhilahafif257@gmail.com>
2026-05-15 17:34:11 +0700

    feat: add ThemeProvider

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,13 @@
+import { useEffect, type ReactNode } from "react";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }, []);
+
+  return <>{children}</>;
+}
